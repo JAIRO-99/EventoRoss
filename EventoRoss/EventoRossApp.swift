9a4e3b826adc1686155e5d5e20a66f7a9c3deea0
JAIRO-99 <jairo.lcelis1999@gmail.com>
2024-04-12 01:18:36 -0500

    Initial Commit

---
+++ EventoRoss/EventoRossApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventoRossApp.swift
+//  EventoRoss
+//
+//  Created by Jairo Laurente Celis on 12/04/24.
+//
+
+import SwiftUI
+
+@main
+struct EventoRossApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
